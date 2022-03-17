8cb88ce757feb893657a0ef072c1a7e14b9d76a1
awaisX64 <awaisali11150@gmail.com>
2022-03-17 16:15:53 +0500

    createdtask

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,5 +3,27 @@ pragma solidity >=0.5.0;
 
 contract TodoList
 {
-    uint public taskCount = 0;  
+  uint public taskCount = 0;
+
+    struct Task{
+    uint id;
+    string content;
+    bool completed;
+    }
+    mapping(uint => Task) public tasks;
+
+    event taskCreated (
+    uint id,
+    string content,
+    bool completed
+    )  ;
+    constructor()  public
+    {
+            createTask("WakeUp Sid");
+    }
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    emit taskCreated(taskCount, _content, false);
+    }
 }
